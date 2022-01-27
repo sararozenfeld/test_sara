de0a4991fadc20854ca09984021e612ba8a93e17
sararozenfeld <89126890+sararozenfeld@users.noreply.github.com>
2022-01-27 15:10:38 +0200

    Update import requests.py

--- test/import requests.py
+++ test/import requests.py
@@ -7,14 +7,12 @@ from urllib.parse import urlencode
 
 class Tests(unittest.TestCase):
 
-    def test_get_carts_cartId_items(self, cartId=None):
-        # GET http://carts.sock-shop-no-p/carts/{cartId}/items (endp 9)
-        cartId = 'Jwu6OzyhQN-s83YFlHSA6eC2NfU7q1Dj' if cartId is None else cartId
-        resp = requests.get("http://carts.sock-shop-no-p" + f'/carts/{cartId}/items')
+    def test_get___ready(self):
+        # GET http://alertmanager-main.monitoring/-/ready (endp 2)
+        resp = requests.get("http://alertmanager-main.monitoring" + '/-/ready')
         resp.raise_for_status()
         
 
 
         # assert resp.status_code in [200]
-        # assert parse("$[*].id").find(resp.json())
-        # assert resp.elapsed.total_seconds() < 0.004474692379085791 # this is based on in-cluster average response time
+        # assert resp.elapsed.total_seconds() < 0.00036930719647210094 # this is based on in-cluster average response time
