c214be5dbc636cd39d13fe0ca75eecacaa21de7e
sararozenfeld <89126890+sararozenfeld@users.noreply.github.com>
2022-01-27 15:31:03 +0200

    k

--- test/import requests.py
+++ test/import requests.py
@@ -13,6 +13,19 @@ class Tests(unittest.TestCase):
         resp.raise_for_status()
         
 
+    def test_get_catalogue_itemId(self, itemId=None):
+        # GET http://catalogue.sock-shop/catalogue/{itemId} (endp 149)
+        itemId = '510a0d7e-8e83-4193-b483-e27e09ddc34d' if itemId is None else itemId
+        resp = requests.get("http://catalogue.sock-shop" + f'/catalogue/{itemId}')
+        resp.raise_for_status()
+        
+
+
+        # assert resp.status_code in [200]
+        # assert parse("$.name").find(resp.json())[0].value == "SuperSport XL"
+        # assert resp.elapsed.total_seconds() < 0.004734253766449238 # this is based on in-cluster average response time
+
+
 
         # assert resp.status_code in [200]
         # assert parse("$.health[*].status").find(resp.json())[0].value == "OK"
