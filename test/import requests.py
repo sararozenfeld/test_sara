c82f5625009b65e5e197847c6bf6b53033c66612
sararozenfeld <89126890+sararozenfeld@users.noreply.github.com>
2022-01-27 15:20:54 +0200

    ddsdaffsdgf
    x

--- test/import requests.py
+++ test/import requests.py
@@ -7,12 +7,13 @@ from urllib.parse import urlencode
 
 class Tests(unittest.TestCase):
 
-    def test_get___ready(self):
-        # GET http://alertmanager-main.monitoring/-/ready (endp 2)
-        resp = requests.get("http://alertmanager-main.monitoring" + '/-/ready')
+    def test_get_health(self):
+        # GET http://catalogue.sock-shop-p/health (endp 6)
+        resp = requests.get("http://catalogue.sock-shop-p" + '/health')
         resp.raise_for_status()
         
 
 
         # assert resp.status_code in [200]
-        # assert resp.elapsed.total_seconds() < 0.00036930719647210094 # this is based on in-cluster average response time
+        # assert parse("$.health[*].status").find(resp.json())[0].value == "OK"
+        # assert resp.elapsed.total_seconds() < 0.00034237665738286443 # this is based on in-cluster average response time
