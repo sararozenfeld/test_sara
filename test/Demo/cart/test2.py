d641ee674e7df86c07214bf63c5f132e3811b4ac
sararozenfeld <89126890+sararozenfeld@users.noreply.github.com>
2022-02-21 14:55:55 +0200

    Update test2.py

--- test/Demo/cart/test2.py
+++ test/Demo/cart/test2.py
@@ -5,15 +5,22 @@ from urllib.parse import urlencode
 # from lxml import html
 
 
-class TestsCatalog(unittest.TestCase):
-
-    def test_get_catalogue_size(self):
-        # GET http://catalogue.sock-shop/catalogue/size (endp 33)
-        qstr = '?' + urlencode({'tags': ''})
-        resp = requests.get("http://catalogue.sock-shop" + '/catalogue/size' + qstr)
+class Tests(unittest.TestCase):
+    
+    url = "http://catalogue.sock-shop"
+    
+    def test_get_catalogue(self, size=None):
+        # GET http://catalogue.sock-shop/catalogue (endp 8)
+        size = '5' if size is None else size
+        qstr = '?' + urlencode({'size': size})
+        resp = requests.get(Tests.url + '/catalogue' + qstr)
         resp.raise_for_status()
         
 
 
         # assert resp.status_code in [200]
-        # assert resp.elapsed.total_seconds() < 0.0000307979170558726 # this is based on in-cluster average response time
+        # assert parse("$[*].tag[*]").find(resp.json())
+        # assert resp.elapsed.total_seconds() < 0.00916664752775956 # this is based on in-cluster average response time
+
+
+   
